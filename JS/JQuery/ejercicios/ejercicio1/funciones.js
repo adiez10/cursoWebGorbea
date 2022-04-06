aea78a0a36fe4a074255fc4e79bdcf01e9c33ef6
PC-15\PCadrian <Adrian>
2022-04-06 14:29:33 +0200

    ultimo ejemplos JQUERY; ejercicios 1 y 2 JQUERY

---
+++ JS/JQuery/ejercicios/ejercicio1/funciones.js
@@ -0,0 +1,24 @@
+$(document).ready(inicializarPrograma);
+
+function inicializarPrograma(){
+  $("#parraf1").click(presionarParrafo1);
+  $("#parraf2").click(presionarParrafo2);
+  $("#parraf2").dblclick(presionarParrafo22);
+}
+
+function presionarParrafo1(){
+  alert("Se ha presionado el parrafo 1");
+  $("#parraf1").css("color","red");
+}
+
+function presionarParrafo2(){
+  //alert("Se ha presionado el parrafo 2");
+  $("#parraf2").removeClass("azul");
+  $("#parraf2").addClass("verde");  
+}
+
+function presionarParrafo22(){
+  //alert("Se ha presionado el parrafo 2");
+  $("#parraf2").removeClass("verde");
+  $("#parraf2").addClass("azul");
+}
