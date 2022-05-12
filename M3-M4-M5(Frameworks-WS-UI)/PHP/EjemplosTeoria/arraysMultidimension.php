c7bedd3905b304dbc147ce7bbcd500d6f4b7fdc1
PC-15\PCadrian <Adrian>
2022-05-12 13:09:23 +0200

    ejemplo clase

--- M3-M4-M5(Frameworks-WS-UI)/PHP/EjemplosTeoria/arraysMultidimension.php
+++ M3-M4-M5(Frameworks-WS-UI)/PHP/EjemplosTeoria/arraysMultidimension.php
@@ -1,29 +1,37 @@
 <!DOCTYPE html>
 <html lang="es" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Probando arrays multidimensionales</title>
-  </head>
-  <body>
-    <h1>Array multidimensionales</h1>
-    <?php
-    $booksMulti = array(
-      array('title'=>'php manual','editor'=>'Xeeer','author'=>'Aitor'),
-      array('title'=>'perl manual','editor'=>'Yelmo','author'=>'Barbara'),
-      array('title'=>'C manual','editor'=>'Zeta','author'=>'Cesar'),
-      array('title'=>'HTML manual','editor'=>'Blabla','author'=>'Javier'),
-      array('title'=>'El quijote','editor'=>'Planeta','author'=>'Cervantes')
-    );
+<head>
+  <meta charset="utf-8">
+  <title>Probando arrays multidimensionales</title>
+</head>
+<body>
+  <h1>Array multidimensionales bucle FOR</h1>
+  <?php
+  $booksMulti = array(
+    array('title'=>'php manual','editor'=>'Xeeer','author'=>'Aitor'),
+    array('title'=>'perl manual','editor'=>'Yelmo','author'=>'Barbara'),
+    array('title'=>'C manual','editor'=>'Zeta','author'=>'Cesar'),
+    array('title'=>'HTML manual','editor'=>'Blabla','author'=>'Javier'),
+    array('title'=>'El quijote','editor'=>'Planeta','author'=>'Cervantes')
+  );
 
-    for($j=0; $j<count($booksMulti);$j++){
-      echo $j.'-st book, titulo: ' .$booksMulti[$j]['title'];
-      echo ' | Autor: '.$booksMulti[$j]['author'];
-      echo ' | Editor: '.$booksMulti[$j]['editor'];
-      echo '<br>';
-    }
-
-    echo $booksMulti[2]['title'];
-
-    ?>
-  </body>
+  for($j=0; $j<count($booksMulti);$j++){
+    echo $j.'-st book, titulo: ' .$booksMulti[$j]['title'];
+    echo ' | Autor: '.$booksMulti[$j]['author'];
+    echo ' | Editor: '.$booksMulti[$j]['editor'];
+    echo '<br>';
+  }
+  //imprime el titulo del array que está en la posición 2
+  echo $booksMulti[2]['title'];
+  ?>
+  <h1>Array multidimensionales bucle FOREACH</h1>
+  <?php
+  foreach ($booksMulti as $row) {
+    echo '<br>';
+    echo 'Titulo: '.$row['title'];
+    echo ' Autor: '.$row['author'];
+    echo ' Editor: '.$row['editor'];
+  }
+  ?>
+</body>
 </html>
