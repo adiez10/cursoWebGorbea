ed17b361ce35f5eb9af4ca291c1c5c46d597f75c
PC-15\PCadrian <Adrian>
2022-05-12 13:49:24 +0200

    ejercicios PHP

---
+++ M3-M4-M5(Frameworks-WS-UI)/PHP/Ejercicios/ej1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Ejercicio 1</title>
+  </head>
+  <body>
+    <?php
+      //1. Muestra hola mundo por pantalla. 
+      print '<p style="color:red;font-size:46px">';
+      print '¡¡¡Hola mundo!!!';
+      print '</p>';
+
+      echo '<p style="color:red;font-size:46px">';
+      echo '¡¡¡Hola mundo!!!';
+      echo '</p>';
+    ?>
+  </body>
+</html>
