4f99fdf4ea9e91f2cbfe0ae985c630648b6a92e8
PC-15\PCadrian <Adrian>
2022-05-19 09:31:28 +0200

    Proyecto ranking primeros pasos

---
+++ M3-M4-M5(Frameworks-WS-UI)/PHP/ranking/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <?php
+      require_once 'mod/header.php';
+    ?>
+    <link href="css/estilos.css" rel="stylesheet" type= "text/css">
+  </head>
+  <body>
+
+
+  </body>
+  <?php
+    require_once 'mod/footer.php';
+  ?>
+</html>
---
+++ M3-M4-M5(Frameworks-WS-UI)/PHP/ranking/mod/footer.php
@@ -0,0 +1,5 @@
+<footer class="pie">
+  <p class="pieparraf">Author: Adrián DC</p>
+  <img class="imgfooter" src="images/icon48.png">
+  <p class="pieparraf"><a href="mailto:adiez@egibide.org">adiez@egibide.org</a></p>
+</footer>
