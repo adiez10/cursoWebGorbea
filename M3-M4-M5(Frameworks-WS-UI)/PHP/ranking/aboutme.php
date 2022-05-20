55173ed0b0bfbe140684429af2e493a0e01b8262
PC-15\PCadrian <Adrian>
2022-05-20 09:07:12 +0200

    ranking web

---
+++ M3-M4-M5(Frameworks-WS-UI)/PHP/ranking/aboutme.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<head>
+  <?php
+    require_once 'mod/header.php';
+  ?>
+  <link href="css/estilos.css" rel="stylesheet" type= "text/css">
+</head>
+  <body>
+    <section class="sectionabout">
+      <div class="divabout">
+        <img class="imgabout" src="">
+        <h2 class="h2about">Introducción</h2>
+        <p class="pabout">Mi nombre es BLALBA LBALBA dfasfdsalbalsdlf</p>
+        <p class="pabout">Me gustan las BLALBA LBALBA dfasfdsalbalsdlf</p>
+        <br>
+        <h2 class="h2about">De donde soy</h2>
+        <p class="pabout">Naci en un sitio y ahora vivo en otro, aunque he vivido en varios sitios.</p>
+        <br>
+        <h2 class="h2about">Experiencia profesional</h2>
+        <p class="pabout">He trabajado en empresas como BLALBA LBALBA dfasfdsalbalsdlf</p>
+        <p class="pabout">Actualmente me dedico a BLALBA LBALBA dfasfdsalbalsdlf</p>
+        <br>
+        <h2 class="h2about">Objetivo de la web</h2>
+        <p class="pabout">Esta web ha sido creada para BLALBA LBALBA dfasfdsalbalsdlf</p>        
+      </div>
+    </section>
+  </body>
+
+
+  <?php
+    require_once 'mod/footer.php';
+  ?>
+</html>
--- M3-M4-M5(Frameworks-WS-UI)/PHP/ranking/index.php
+++ M3-M4-M5(Frameworks-WS-UI)/PHP/ranking/index.php
@@ -8,6 +8,35 @@
   </head>
   <body>
 
+    <section id="sectionid">
+      <div class="addArticulo">
+        <h2>Los mejores XYZ</h2>
+        <button class="btn btn-primary">
+          <a href="#">Añadir XYZ</a>
+        </button>
+      </div>
+
+      <div class="grid-container">
+        <div class="celdaRank"><h3>1º</h3></div>
+        <div class="celdaNota"><h2 class="h2Nota">9.5</h2></div>
+        <div class="celdaNombreGrupo"><h4>LA MODA</h4></div>
+        <div class="celdaNombreEstilo"><p>Folk-Rock</p></div>
+        <div class="celdaAnho"><p>2010</p></div>
+        <div class="celdaBoton">
+          <button class="btn btn-secondary">
+            <a href="#">Valorar</a>
+          </button>
+        </div>
+        <div class="celdaDescripTitle">
+          <h5 class="description">Descripción</h5>
+        </div>
+        <div class="celdaDescrip">
+          <p class="description">
+            Lorem ipsum tralali Lorem ipsum tralali Lorem ipsum tralali
+          </p>
+        </div>
+      </div>
+    </section>
 
   </body>
   <?php
